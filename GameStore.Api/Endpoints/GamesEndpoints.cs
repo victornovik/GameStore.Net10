0189d7b9f4078b251de98a574cef7b8d97fd815b
victornovik <victor.novik@mail.ru>
2026-01-06 21:57:46 +0300

    Move all endpoints to extension method

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,53 @@
+﻿using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpoint = "GetGame";
+
+    private static readonly List<GameDto> games = [
+        new(1, "Street Fighter II", "Fighting", 19.99m, new DateOnly(1992,7,15)),
+        new(2, "Final Fantasy VII", "RPG", 69.99m, new DateOnly(2024,2,29)),
+        new(3, "Astro Bot", "Platformer", 59.99m, new DateOnly(2024,9,6))
+    ];
+
+    public static void MapGamesEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", () => "dlrow olleh");
+
+        var group = app.MapGroup("/games");
+
+        group.MapGet("/", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            var found = games.Find(g => g.Id == id);
+            return found != null ? Results.Ok(found) : Results.NotFound();
+        }).WithName(GetGameEndpoint);
+
+        group.MapPost("/", (CreateGameDto g) =>
+        {
+            var newGame = new GameDto(games.Count + 1, g.Name, g.Genre, g.Price, g.ReleaseDate);
+            games.Add(newGame);
+            return Results.CreatedAtRoute(GetGameEndpoint, new { id = newGame.Id }, newGame);
+        });
+
+        group.MapPut("/{id}", (int id, UpdateGameDto changeling) =>
+        {
+            const int NotFound = -1;
+            var index = games.FindIndex(g => g.Id == id);
+            if (index == NotFound)
+                return Results.NotFound();
+
+            games[index] = new GameDto(id, changeling.Name, changeling.Genre, changeling.Price, changeling.ReleaseDate);
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(g => g.Id == id);
+            return Results.NoContent();
+        });
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,35 +1,7 @@
-using GameStore.Api.Dtos;
-
-const string GetGameEndpointName = "GetGame";
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<GameDto> games = [
-    new(1, "Street Fighter II", "Fighting", 19.99m, new DateOnly(1992,7,15)),
-    new(2, "Final Fantasy VII", "RPG", 69.99m, new DateOnly(2024,2,29)),
-    new(3, "Astro Bot", "Platformer", 59.99m, new DateOnly(2024,9,6))
-];
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/games", () => games);
-app.MapGet("/games/{id}", (int id) => games.Find(g => g.Id == id)).WithName(GetGameEndpointName);
-app.MapPost("/games", (CreateGameDto g) =>
-{
-    var newGame = new GameDto(games.Count + 1, g.Name, g.Genre, g.Price, g.ReleaseDate);
-    games.Add(newGame);
-    return Results.CreatedAtRoute(GetGameEndpointName, new {id = newGame.Id}, newGame);
-});
-app.MapPut("games/{id}", (int id, UpdateGameDto changeling) =>
-{
-    var index = games.FindIndex(g => g.Id == id);
-    games[index] = new GameDto(id, changeling.Name, changeling.Genre, changeling.Price, changeling.ReleaseDate);
-    return Results.NoContent();
-});
-app.MapDelete("games/{id}", (int id) =>
-{
-    games.RemoveAll(g => g.Id == id);
-    return Results.NoContent();
-});
-
+app.MapGamesEndpoints();
 app.Run();
